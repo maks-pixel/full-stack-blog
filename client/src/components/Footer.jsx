3ba8b3100c2340e437a80159cfe6d3d6082fccc7
Makda Girmay <makdagirmay4@gmail.com>
2023-02-23 12:33:31 -0500

    footer display design

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../img/logo.png';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <img src={Logo} alt="" />
+            <span>Made with hate <b>React.js</b>.</span>
+        </footer>
     )
 }
 
