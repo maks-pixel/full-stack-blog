ddee65aa11942ba548267951366109c85004d776
Makda Girmay <makdagirmay4@gmail.com>
2023-03-01 13:34:16 -0500

    adding the single post styling and page formating

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import bed from '../img/bed.jpg'
+import beach from '../img/beach.jpg'
+import roofs from '../img/roofs.jpg'
+import livingroom from '../img/livingroom.jpg' 
+import desk from '../img/desk.jpg'
 
 const Home = () => {
+    
+        const posts = [
+            {
+                id: 1,
+                title:"lorem ipsum",
+                desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                img:bed
+            },
+            {
+                id: 2,
+                title:"lorem ipsum",
+                desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                img:beach
+            },
+            {
+                id: 3,
+                title:"lorem ipsum",
+                desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                img:roofs
+            },
+            {
+                id: 4,
+                title:"lorem ipsum",
+                desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                img:livingroom
+            },
+            {
+                id: 5,
+                title:"lorem ipsum",
+                desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                img: desk
+            }
+        ];
     return (
-        <div>Home</div>
+        <div className='home'>
+            <div className='posts'>
+                 {posts.map((post) =>(
+                    <div className='post' key={post.id}> 
+                        <div className='img'>
+                            <img src={post.img} alt="pics" />
+                            
+                        </div>
+                        <div className="content">
+                            <Link className='link' to={`/post/${post.id}`}>
+                                <h1>{post.title}</h1>
+                             </Link>
+                            <p>{post.desc}</p>
+                            <button>Read More</button>
+                        </div>
+                    </div>
+                ))
+                }
+            </div>
+        </div>
     )
 }
 
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import cats from "../img/cats.jpg"
+import smile from "../img/smile.jpg"
+import Edit from "../img/edit.png"
+import Delete from "../img/delete.png"
 const Single = () => {
     return (
-        <div>Single</div>
+        <div className='single'>
+            <div className='content'>
+            <img src= {cats} alt="" />
+            <div className="user">
+                <img src={smile} alt=""></img>
+                <div className="info">
+                    <span> Maks</span>
+                    <p> posted 2 days ago</p>
+                </div>
+                <diiv className="edit">
+                    <Link to={`write?edit=2`}>
+                    <img src={Edit} alt="" />
+                    </Link>
+                    <img src={Delete} alt="" />
+                </diiv>
+            </div>
+            </div>
+            <div className='menu'>m</div>
+
+        </div>
     )
 }
 
